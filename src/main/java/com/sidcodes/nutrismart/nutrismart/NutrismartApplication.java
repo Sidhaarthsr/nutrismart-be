3a07682234839685c3f604f40afb6f1a42e6b140
Sidhaarthsr <srsidhaarth@gmail.com>
2025-01-09 15:07:04 -0500

    NS - SpringBoot Backend - initial commit

---
+++ src/main/java/com/sidcodes/nutrismart/nutrismart/NutrismartApplication.java
@@ -0,0 +1,13 @@
+package com.sidcodes.nutrismart.nutrismart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrismartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrismartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sidcodes/nutrismart/nutrismart/NutrismartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidcodes.nutrismart.nutrismart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrismartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
